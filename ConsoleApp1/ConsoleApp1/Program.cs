6d87f2ea8e34117d6e002a0a52e304ed650c1376
jikuma <jikuma@microsoft.com>
2018-12-19 11:25:34 -0800

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Environment.GetEnvironmentVariable("Release.ReleaseName") ?? $"Manual-{Environment.MachineName}";
+            Console.WriteLine($"Hello World! {name}");
+        }
+    }
+}
